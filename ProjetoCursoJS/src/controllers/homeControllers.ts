86a7e4354281bd854076a7af78f79e68bd393cb0
Sergio Luiz <sergiolmg04@hotmail.com>
2022-07-16 18:40:30 -0300

    Consegui acertar o código e criar o objeto com os dados do notebook

--- ProjetoCursoJS/src/controllers/homeControllers.ts
+++ ProjetoCursoJS/src/controllers/homeControllers.ts
@@ -1,15 +1,76 @@
+import axios from "axios";
+import * as cheerio from "cheerio";
 import { Request, Response } from "express";
-import * as puppeteer from "./exemple"
 
-export const FuncaoGetHome = (req: Request, res: Response)=>{//página padrão do server
-    let user:string = "Sérgio";
+export const FuncaoGetHome = async (req:Request, res:Response)=>{//página padrão do server
+    const HTML = await axios.get('https://webscraper.io/test-sites/e-commerce/allinone/computers/laptops');
+    console.log(HTML);
+    const $ = cheerio.load(HTML.data);
+    const notebook:any = $('.caption');
+    let NotbooksLenovo = [];
+    console.log(notebook);
+    for(let i of notebook){
+      console.log(i);
+   let dadosNotbooks = {
+    Preco: parseFloat(i.children[1].children[0].data.replace("$","")),
+    Link: "https://webscraper.io"+i.childNodes[3].childNodes[1].attribs.href,
+    Nome: i.childNodes[3].childNodes[1].attribs.title
+    }
+    console.log("oi");
+    /*if(dadosNotbooks.Nome contains "Lenovo"){ falta filtrar isso e salvar numa lista que vai ser um array e vou ordenar pelo preço
+      x=1;
+    }*/
+  };
+};
+    
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  /* const browser = await puppeteer.launch({
+      executablePath: 'C:/Program Files (x86)/Microsoft/Edge/Application/msedge.exe',
+      headless: false
+    });
+    const page = await browser.newPage();
+    await page.goto('https://webscraper.io/test-sites/e-commerce/allinone/computers/laptops');
+    let conteudo:any;
+    try{
+      conteudo = await page.$$('.caption');
+      //const elemento = await page.$eval('childNodes', ele => ele.textContent);
+      //console.log("Heading text: "+ elemento);
+
+      /*conteudo = await page.evaluate(() => {
+      return {
+        title: Array.from(document.querySelectorAll('[title]')).filter(title => Object(title).firstChild.data.toString().toUpperCase().includes('LENOVO'))
+      };
+    });
+    }
+  catch(e){
+    console.log(e);
+  }
+    let novoConteudo = Object.values(conteudo);
+    await browser.close();
+    for (let i in novoConteudo){
+    console.log("eba" + novoConteudo[i]);
+  }
+    //console.log(conteudo.title.length);
+    //res.render("pages/home");
+    //console.log("Se deu certo, vai aparecer " );
+    /*let user:string = "Sérgio";
     let menu:string[] = ["Constrole de Materiais", "Aderência da Programação", "Andamento das Avaliações"];
     let query:string = "Ainda não recebi nada";
-    /*const func = funcao;
-    function funcao(){
-        puppeteer.pupp;
-      }*/
-    res.render("pages/home", {user, menu, query}/*, funcao*/); //manda informações para a página
-    console.log("Query string",req.query);
-    query = req.query.nome as string;
-};
+    /*, funcao); //manda informações para a página*/
+   
+    //query = req.query.nome as string;*/
+//};
