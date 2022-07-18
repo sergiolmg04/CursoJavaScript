594892c6c17add24e2f66cc0759d477a2e92742e
Sergio Luiz <sergiolmg04@hotmail.com>
2022-07-17 23:17:09 -0300

    Consegui colocar em um array, ele já está ordenado, mas não usei o sort, veio ordenado pelo site, falta criar essa função

--- ProjetoCursoJS/src/controllers/homeControllers.ts
+++ ProjetoCursoJS/src/controllers/homeControllers.ts
@@ -1,70 +1,63 @@
+//importando as bibliotecas e types necessários
 import axios from "axios";
 import * as cheerio from "cheerio";
 import { Request, Response } from "express";
 
-export const FuncaoGetHome = async (req:Request, res:Response)=>{//página padrão do server
-    const HTML = await axios.get('https://webscraper.io/test-sites/e-commerce/allinone/computers/laptops');
-    console.log(HTML);
-    const $ = cheerio.load(HTML.data);
-    const notebook:any = $('.caption');
-    let NotbooksLenovo = [];
-    console.log(notebook);
-    for(let i of notebook){
-      console.log(i);
-   let dadosNotbooks = {
-    Preco: parseFloat(i.children[1].children[0].data.replace("$","")),
-    Link: "https://webscraper.io"+i.childNodes[3].childNodes[1].attribs.href,
-    Nome: i.childNodes[3].childNodes[1].attribs.title
-    }
-    console.log("oi");
-    /*if(dadosNotbooks.Nome contains "Lenovo"){ falta filtrar isso e salvar numa lista que vai ser um array e vou ordenar pelo preço
-      x=1;
-    }*/
-  };
-};
-    
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  /* const browser = await puppeteer.launch({
-      executablePath: 'C:/Program Files (x86)/Microsoft/Edge/Application/msedge.exe',
-      headless: false
-    });
-    const page = await browser.newPage();
-    await page.goto('https://webscraper.io/test-sites/e-commerce/allinone/computers/laptops');
-    let conteudo:any;
-    try{
-      conteudo = await page.$$('.caption');
-      //const elemento = await page.$eval('childNodes', ele => ele.textContent);
-      //console.log("Heading text: "+ elemento);
+//exportando a funçao GetHome para a página principal do servidor
+export const FuncaoGetHome = async (req:Request, res:Response)=>{
 
-      /*conteudo = await page.evaluate(() => {
-      return {
-        title: Array.from(document.querySelectorAll('[title]')).filter(title => Object(title).firstChild.data.toString().toUpperCase().includes('LENOVO'))
-      };
-    });
-    }
-  catch(e){
-    console.log(e);
-  }
-    let novoConteudo = Object.values(conteudo);
-    await browser.close();
-    for (let i in novoConteudo){
-    console.log("eba" + novoConteudo[i]);
-  }
-    //console.log(conteudo.title.length);
+//A variável HTML recebe todo o conteúdo HTML do URL em forma de string
+const HTML = await axios.get('https://webscraper.io/test-sites/e-commerce/allinone/computers/laptops');
+  console.log(HTML); //apenas para ver como a HTML foi recebida
+
+//Utilizando a lib Cheerio para quebrar o string HTML e poder fazer pesquisas
+const $ = cheerio.load(HTML.data);
+
+//Varia´vel que vai receber todos os itens que contem a classe caption
+const notebook:any = $('.caption');
+
+//variável que vai receber o array dos notebooks da lenovo
+let NotebooksLenovo:any = [];
+
+//apenas para ver o valor que notebook recebeu
+console.log(notebook);
+
+//loop para criar o objeto com os dados do notebook
+for(let i of notebook){
+  console.log(i); //mostrar o valor que i está recebendo
+
+//declaração da variável que vai receber o objeto com os dados Preço, Link e Nome de todos os notebooks
+let dadosNotebooks = {
+  Preco: parseFloat(i.children[1].children[0].data.replace("$","")),
+  Link: "https://webscraper.io"+i.childNodes[3].childNodes[1].attribs.href,
+  Nome: i.childNodes[3].childNodes[1].attribs.title
+}
+
+//deixando o title maiúsculo
+dadosNotebooks.Nome = dadosNotebooks.Nome.toUpperCase()
+
+//apenas para ver como o objeto foi criado
+  console.log("oi");
+
+console.log(dadosNotebooks.Nome.includes("ASUS"));
+//comparação para separar os notebooks que são lenovo dos outros
+if(dadosNotebooks.Nome.includes("LENOVO")){ //falta ordenar pelo preço
+  let x=1; //apenas para saber se o if está funcionando
+  NotebooksLenovo.push(dadosNotebooks);
+  console.log(x); //para saber se x recebendo 1
+  console.log(NotebooksLenovo);//ver notebooks lenovo
+}
+else {
+  let y=2; //apenas para saber se o else está funcionando
+  console.log(y); //para saber se y recebendo 2
+}
+
+//Ordenar o array
+
+
+}; //fecha o for
+};// fecha a FuncaoGetHome
+
     //res.render("pages/home");
     //console.log("Se deu certo, vai aparecer " );
     /*let user:string = "Sérgio";
